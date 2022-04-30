3db1635e50931340fb4c9b44daaaab27f892ed80
林柏禎 <65914020+brian123000@users.noreply.github.com>
2022-04-30 23:29:50 +0800

    Update main.cpp

--- src/week11/hw01/main.cpp
+++ src/week11/hw01/main.cpp
@@ -4,7 +4,32 @@
 using namespace std;
 
 // add your code here
-
+void printAll(int n)
+{	
+        for (int i = 1; i <= n; i++)
+            cout << setw(4) << i;   
+} 
+void printEven(int n)
+{
+        cout << "Even    : ";
+        for (int i = 1; i <= n; i++)
+            if (i % 2 == 0)
+                cout << setw(4) << i;  
+}
+void printOdd(int n)
+{	
+        cout << "Odd     : ";
+        for (int i = 1; i <= n; i++)
+            if (i % 2 != 0)
+                cout << setw(4) << i;
+}
+void printDiv4(int n) 
+{
+        cout << "Div. 4  : ";
+        for (int i = 1; i <= n; i++)
+            if (i % 4 == 0)
+                cout << setw(4) << i;
+}
 // -----vv----- 不得修改『以下』的程式 -----vv-----
 
 int main()
